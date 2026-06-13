ec93d1e0721906535bdc119d11c922ca2d5c3f22
riyajivani85 <riyajivani85@gmail.com>
2026-06-13 22:17:29 +0530

    module-3 exception

---
+++ classes/module-3/Exception/try_else.py
@@ -0,0 +1,8 @@
+try:
+    a=int(input("Enter A:"))
+    b=int(input("Enter B:"))
+    print("Sum:",a+b)
+except Exception as e:
+    print(e)
+else:
+    print("else part execute")
---
+++ classes/module-3/Exception/try_execpt.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+try:
+    print("sum :", a+B)
+except Exception as e:
+    print(e)
+
+x=10
+y=20
+print("sub :",a-b)
